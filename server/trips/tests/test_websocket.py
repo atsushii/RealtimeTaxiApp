ddad5a65655c9706c83cf253ce211e56cbc8b557
Atsushi <atsushimiyamoto07@gmail.com>
2021-02-20 14:36:35 -0800

    wrote test for websocket connection

---
+++ server/trips/tests/test_websocket.py
@@ -0,0 +1,25 @@
+import pytest
+from channels.testing import WebsockectCommunicator
+
+from taxi.routing import application
+
+
+TEST_CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels.layers.InMemoryChannelLayer',
+    }
+}
+
+
+@pytest.mark.asyncio
+class TestWebSocket:
+    async def test_can_connect_to_server(self, settings):
+        settings.CHANNEL_LAYERS = TEST_CHANNEL_LAYERS
+        communicator = WebsockectCommunicator(
+            application=application,
+            path='/taxi/'
+        )
+        connected, _ = await communicator.connect()
+        assert connected is True
+        await communicator.disconnected()
+
