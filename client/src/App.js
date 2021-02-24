be2bd24cb434bf77572dc0354f2de6938c0e89be
Atsushi <atsushimiyamoto07@gmail.com>
2021-02-23 21:15:50 -0800

    Added router for form

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Route, Link, Switch } from 'react-router-dom';
+
+import SignUp from './components/SignUp';
+import LogIn from './components/Login';
+
+function App() {
+  return (
+    <Switch>
+        <Route exact path='/' render={() => (
+          <div>
+            <h1>Taxi</h1>
+            <Link to='/sign-up'>Sign up</Link>
+            <Link to='/log-in'>Log in</Link>
+          </div>
+        )} />
+        <Route path='/sign-up' component={SignUp} />
+        <Route path='/log-in' component={LogIn} />
+      </Switch>
+  );
+}
+
+export default App;
