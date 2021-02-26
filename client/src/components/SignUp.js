f90ee6b9265202fafce8b518e6a5b9e161dba326
Atsushi <atsushimiyamoto07@gmail.com>
2021-02-25 20:27:33 -0800

    Pass sign up and login test

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,8 +1,18 @@
-import React from 'react';
-import { Breadcrumb, Card, Col, Row } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Formik } from 'formik';
+import { Breadcrumb, Card, Col, Row, Form, Button } from 'react-bootstrap';
+import { Link, Redirect } from 'react-router-dom';
 
 function SignUp(props) {
+
+    const [isSubmitted, setSubmitted] = useState(false);
+
+    const onSubmit = (values, actions) => setSubmitted(true)
+
+    if (isSubmitted) {
+        return <Redirect to='/log-in' />
+    }
+
     return (
         <Row>
             <Col lg={12}>
@@ -12,7 +22,82 @@ function SignUp(props) {
                 </Breadcrumb>
                 <Card>
                     <Card.Header>Sign up</Card.Header>
-                    <Card.Body></Card.Body>
+                    <Card.Body>
+                        <Formik
+                            initialValues={{
+                                username: '',
+                                firstName: '',
+                                lastName: '',
+                                password: '',
+                                group: 'rider',
+                                photo: []
+                            }}
+                            onSubmit={onSubmit}
+                        >
+                            {({
+                                handleChange,
+                                handleSubmit,
+                                values
+                            }) =>(
+                                <Form noValidate onSubmit={handleSubmit}>
+                                    <Form.Group controlId='username'>
+                                    <Form.Label>Username</Form.Label>
+                                        <Form.Control
+                                            name='username'
+                                            onChange={handleChange}
+                                            values={values.username}
+                                        />
+                                    </Form.Group>
+                                    <Form.Group controlId='firstName'>
+                                        <Form.Label>First name</Form.Label>
+                                        <Form.Control
+                                            name='firstName'
+                                            onChange={handleChange}
+                                            values={values.firstName}
+                                        />
+                                    </Form.Group>
+                                    <Form.Group controlId='lastName'>
+                                        <Form.Label>Last name</Form.Label>
+                                        <Form.Control
+                                            name='lastName'
+                                            onChange={handleChange}
+                                            values={values.lastName}
+                                        />
+                                    </Form.Group>
+                                    <Form.Group controlId='password'>
+                                        <Form.Label>Password</Form.Label>
+                                        <Form.Control
+                                            name='password'
+                                            onChange={handleChange}
+                                            values={values.password}
+                                        />
+                                    </Form.Group>
+                                    <Form.Group controlId='group'>
+                                        <Form.Label>Group</Form.Label>
+                                        <Form.Control
+                                            as='select'
+                                            name='group'
+                                            onChange={handleChange}
+                                            values={values.group}
+                                        >
+                                            <option value='rider'>Rider</option>
+                                            <option value='driver'>Driver</option>
+                                        </Form.Control>
+                                    </Form.Group>
+                                    <Form.Group controlId='photo'>
+                                        <Form.Label>Photo</Form.Label>
+                                        <Form.Control
+                                            name='photo'
+                                            onChange={handleChange}
+                                            type='file'
+                                            values={values.photo}
+                                        />
+                                    </Form.Group>                                
+                                    <Button block type='submit' variant='primary'>Sign up</Button>
+                                </Form>
+                            )}
+                        </Formik>
+                    </Card.Body>
                     <p className='mt-3 text-center'>
                         Already have an account? <Link to='/log-in'>Log in!</Link>
                     </p>
