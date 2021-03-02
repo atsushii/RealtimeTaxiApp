01f2df2bd9d3aaa2830b8256d8fe1fad0e2e667a
Atsushi <atsushimiyamoto07@gmail.com>
2021-03-02 14:21:29 -0800

    create dashboard for rider

---
+++ client/cypress/integration/rider.spec.js
@@ -0,0 +1,35 @@
+const faker = require('faker');
+
+const randomEmail = faker.internet.email()
+
+describe('The rider dashboard', function () {
+    it('Cannnot be visited if the user is not a rider', function () {
+        cy.intercept('POST', 'sign_up').as('signUp');
+        cy.intercept('POST', 'log_in').as('logIn');
+
+        cy.visit('/#/sign-up');
+        cy.get('input#username').type(randomEmail);
+        cy.get('input#firstName').type('Atsushi');
+        cy.get('input#lastName').type('Miymoto');
+        cy.get('input#password').type('pAssw0rd', { log: false });
+        cy.get('select#group').select('driver');
+
+        // handle file upload
+        cy.get('input#photo').attachFile('images/photo.jpg');
+        cy.get('button').contains('Sign up').click();
+        cy.wait('@signUp');
+        cy.hash().should('eq', '#/log-in');
+
+        // Log in
+        cy.visit('/#/log-in');
+        cy.get('input#username').type(randomEmail);
+        cy.get('input#password').type('pAssw0rd', { log: false });
+        cy.get('button').contains('Log in').click();
+        cy.hash().should('eq', '#/');
+        cy.get('button').contains('Log out');
+        cy.wait('@logIn');
+
+        cy.visit('/#/rider');
+        cy.hash().should('eq', '#/')
+    });
+});
